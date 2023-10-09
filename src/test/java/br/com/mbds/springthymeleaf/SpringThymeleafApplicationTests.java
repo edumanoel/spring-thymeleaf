02d016c75a6db742e2350a848cf71ef59e2c2354
Eduardo Belem <leal@pm.ba.gov.br>
2023-10-09 19:13:15 -0300

    first commit

---
+++ src/test/java/br/com/mbds/springthymeleaf/SpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mbds.springthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
