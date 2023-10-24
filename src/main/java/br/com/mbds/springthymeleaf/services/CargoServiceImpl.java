1f6bf694ee28e7760b1d7b693cf199d8457ec5f3
Eduardo Belem <leal@pm.ba.gov.br>
2023-10-24 19:35:59 -0300

    implemented functions: create, list, edit, delete in view cargos

--- src/main/java/br/com/mbds/springthymeleaf/services/CargoServiceImpl.java
+++ src/main/java/br/com/mbds/springthymeleaf/services/CargoServiceImpl.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.mbds.springthymeleaf.entities.Cargo;
+import br.com.mbds.springthymeleaf.exceptions.DataBaseException;
+import br.com.mbds.springthymeleaf.exceptions.NotFoundException;
 import br.com.mbds.springthymeleaf.repositories.CargoRepository;
 
 @Service
@@ -16,16 +20,28 @@ public class CargoServiceImpl implements CargoService {
 	@Autowired
 	private CargoRepository repository;
 
-	@Transactional
+	@Transactional(propagation = Propagation.SUPPORTS)
 	@Override
 	public Cargo save(Cargo entity) {
-		return repository.save(entity);
+		try {
+			return repository.save(entity);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 
-	@Transactional
+	@Transactional(propagation = Propagation.SUPPORTS)
 	@Override
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			if (repository.existsById(id)) {
+				repository.deleteById(id);
+			} else {
+				throw new NotFoundException(id);
+			}
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 
 	@Override
--- src/main/java/br/com/mbds/springthymeleaf/web/controllers/CargoController.java
+++ src/main/java/br/com/mbds/springthymeleaf/web/controllers/CargoController.java
@@ -1,21 +1,66 @@
 package br.com.mbds.springthymeleaf.web.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.mbds.springthymeleaf.entities.Cargo;
+import br.com.mbds.springthymeleaf.entities.Departamento;
+import br.com.mbds.springthymeleaf.services.CargoService;
+import br.com.mbds.springthymeleaf.services.DepartamentoService;
 
 @Controller
-@RequestMapping("/cargos")
+@RequestMapping("cargos")
 public class CargoController {
 
-	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	@Autowired
+	private CargoService cargoService;
+
+	@Autowired
+	private DepartamentoService departamentoService;
+
+	@GetMapping("cadastrar")
+	public String cadastrar(Cargo cargo) {
 		return "/cargo/cadastro";
 	}
 
-	@GetMapping("/listar")
-	public String listar() {
+	@GetMapping("listar")
+	public String listar(ModelMap model) {
+		model.addAttribute("listaCargos", cargoService.findAll());
 		return "/cargo/lista";
+	}
+
+	@PostMapping("salvar")
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.save(cargo);
+		attr.addFlashAttribute("success", "Cargo salvo com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+
+	@GetMapping("editar/{id}")
+	public String editar(@PathVariable Long id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.findById(id));
+		return "/cargo/cadastro";
+	}
+
+	@GetMapping("excluir/{id}")
+	public String excluir(@PathVariable Long id, ModelMap model) {
+		cargoService.delete(id);
+		model.addAttribute("success", "Cargo excluído com sucesso.");
+		return listar(model);
+	}
+
+	@ModelAttribute("listaDepartamentos")
+	public List<Departamento> getListaDepartamentos() {
+		return departamentoService.findAll();
 	}
 
 }
