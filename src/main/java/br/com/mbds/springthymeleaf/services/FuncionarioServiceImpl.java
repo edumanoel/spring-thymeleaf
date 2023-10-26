2ec4e0f1a7552897490d0a4eb52bedd2124f6d7e
Eduardo Belem <leal@pm.ba.gov.br>
2023-10-26 17:05:04 -0300

    implemented functions: create, list, edit, delete in view funcionarios

--- src/main/java/br/com/mbds/springthymeleaf/services/FuncionarioServiceImpl.java
+++ src/main/java/br/com/mbds/springthymeleaf/services/FuncionarioServiceImpl.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.mbds.springthymeleaf.entities.Funcionario;
+import br.com.mbds.springthymeleaf.exceptions.DataBaseException;
+import br.com.mbds.springthymeleaf.exceptions.NotFoundException;
 import br.com.mbds.springthymeleaf.repositories.FuncionarioRepository;
 
 @Service
@@ -16,16 +20,28 @@ public class FuncionarioServiceImpl implements FuncionarioService {
 	@Autowired
 	private FuncionarioRepository repository;
 
-	@Transactional
+	@Transactional(propagation = Propagation.SUPPORTS)
 	@Override
 	public Funcionario save(Funcionario entity) {
-		return repository.save(entity);
+		try {
+			return repository.save(entity);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 
-	@Transactional
+	@Transactional(propagation = Propagation.SUPPORTS)
 	@Override
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			if (repository.existsById(id)) {
+				repository.deleteById(id);
+			} else {
+				throw new NotFoundException(id);
+			}
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 
 	@Override
--- src/main/java/br/com/mbds/springthymeleaf/web/controllers/FuncionarioController.java
+++ src/main/java/br/com/mbds/springthymeleaf/web/controllers/FuncionarioController.java
@@ -1,21 +1,71 @@
 package br.com.mbds.springthymeleaf.web.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.mbds.springthymeleaf.entities.Cargo;
+import br.com.mbds.springthymeleaf.entities.Funcionario;
+import br.com.mbds.springthymeleaf.entities.enums.UF;
+import br.com.mbds.springthymeleaf.services.CargoService;
+import br.com.mbds.springthymeleaf.services.FuncionarioService;
 
 @Controller
-@RequestMapping("/funcionarios")
+@RequestMapping("funcionarios")
 public class FuncionarioController {
 
-	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Autowired
+	private CargoService cargoService;
+
+	@GetMapping("cadastrar")
+	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
 
-	@GetMapping("/listar")
-	public String listar() {
+	@GetMapping("listar")
+	public String listar(ModelMap model) {
+		model.addAttribute("listaFuncionarios", funcionarioService.findAll());
 		return "/funcionario/lista";
 	}
 
+	@PostMapping("salvar")
+	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.save(funcionario);
+		attr.addFlashAttribute("success", "Funcionario salvo com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+
+	@GetMapping("editar/{id}")
+	public String editar(@PathVariable Long id, ModelMap model) {
+		model.addAttribute("funcionario", funcionarioService.findById(id));
+		return "/funcionario/cadastro";
+	}
+
+	@GetMapping("excluir/{id}")
+	public String excluir(@PathVariable Long id, ModelMap model) {
+		funcionarioService.delete(id);
+		model.addAttribute("success", "Funcionario excluído com sucesso.");
+		return listar(model);
+	}
+
+	@ModelAttribute("listaCargos")
+	public List<Cargo> getListaCargos() {
+		return cargoService.findAll();
+	}
+
+	@ModelAttribute("listaUFs")
+	public UF[] getUFs() {
+		return UF.values();
+	}
 }
